dcab41e13d44dfe609138d0a4b8bdcc891a8389e
Vineet Maheshwari <vineet@dakinegroup.com>
2015-08-04 10:44:05 +0530

    .

--- StoresWithSpringAop/src/com/dakinegroup/Test.java
+++ StoresWithSpringAop/src/com/dakinegroup/Test.java
@@ -1,7 +1,11 @@
 package com.dakinegroup;
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.ClassPathResource;  
+import org.springframework.core.io.Resource;  
+  
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
 
 
 public class Test {
@@ -13,5 +17,10 @@ public class Test {
         si.setDescription1("Shirt XL Cotton - Red - Arrow");
         si.setDescription2("Shirt L Cotton - Red - Arrow");
         si.displayInfo();
+	Resource resource = new ClassPathResource("applicationContext.xml");
+        BeanFactory factory = new XmlBeanFactory(resource);
+        
+        Store si2 = (Store)factory.getBean("store1");
+        si2.displayInfo();
     }
 }
