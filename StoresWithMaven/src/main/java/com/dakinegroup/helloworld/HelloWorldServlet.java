447e44832b6962bd821dbe8866ae5537e4efbc6f
Vineet Maheshwari <vineet@dakinegroup.com>
2015-08-17 20:25:17 +0530

    .

--- StoresWithMaven/src/main/java/com/dakinegroup/helloworld/HelloWorldServlet.java
+++ StoresWithMaven/src/main/java/com/dakinegroup/helloworld/HelloWorldServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 
-//@WebServlet(name="hello",urlPatterns={"/hello"})
+@WebServlet(name="hello",urlPatterns={"/hello"})
 public class HelloWorldServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
--- StoresWithMaven/src/main/java/com/dakinegroup/storesApp/StoresApp.java
+++ StoresWithMaven/src/main/java/com/dakinegroup/storesApp/StoresApp.java
@@ -1,12 +1,19 @@
-package com.dakinegroup;
+package com.dakinegroup.storesApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StoresApp {
+public class StoresApp extends SpringBootServletInitializer{
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StoresApp.class);
+    }
+
+    public static void main(String[] args) throws Throwable {
         SpringApplication.run(StoresApp.class, args);
     }
-}
+ }
